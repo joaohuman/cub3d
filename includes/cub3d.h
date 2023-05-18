4e561896862bf1e1ebd4b0377278c754eb01cccd
LucasSAPacheco <lukssapsilva@gmail.com>
2023-05-18 14:11:54 -0300

    Function check_file created

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -5,8 +5,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int check_errors(int argc, char *argv[]);
 int check_extension(char *arg, char *extension);
+int check_file(char *file_name);
 
 #endif
--- sources/check_errors.c
+++ sources/check_errors.c
@@ -2,8 +2,11 @@
 
 int check_errors(int argc, char *argv[]){
     if (argc != 2)
-        return (printf("Error\nWrong number of arguments\n"), 0);
-    check_extension(argv[1], ".cub");
+        return (perror("Error\nWrong number of arguments\n"), -1);
+    if (check_extension(argv[1], ".cub"))
+        return -1;
+    if (check_file(argv[1]))
+        return -1;
     return 0;
 }
 
@@ -17,9 +20,19 @@ int check_extension(char *arg, char *extension)
     while (*aux && *extension)
     {
         if (*aux != *extension)
-            return (perror("Error\nWrong extension\n"), 0);
+            return (perror("Error\nWrong extension\n"), -1);
         aux++;
         extension++;
     }
-    return (1);
+    return (0);
+}
+
+int check_file(char *file_name)
+{
+    int fd;
+
+    fd = open(file_name, O_RDONLY);
+    if (fd == -1)
+        return (perror("Error\n"), -1);
+    return (0);
 }
