c37b9c12325d3ef34f039eabb5c881d019bc271a
joaohuman <joaovictorgrodrigues@gmail.com>
2023-05-19 03:56:46 +0200

    just added some errors messages

--- sources/check_errors.c
+++ sources/check_errors.c
@@ -1,9 +1,15 @@
 #include "../includes/cub3d.h"
 
+int	error_msg(char *msg)
+{
+	printf("Error\n%s\n", msg);
+	return (-1);
+}
+
 int	check_errors(int argc, char *argv[])
 {
 	if (argc != 2)
-		return (perror(""), -1);
+		return (error_msg("Need just 2 args 'cub3d' and 'map_you_chose'"));
 	if (check_extension(argv[1], ".cub"))
 		return (-1);
 	if (check_file(argv[1]))
@@ -21,7 +27,7 @@ int	check_extension(char *arg, char *extension)
 	while (*aux || *extension)
 	{
 		if (*aux != *extension)
-			return (perror("Wrong Extension Type"), -1);
+			return (error_msg("Wrong extension type"));
 		aux++;
 		extension++;
 	}
@@ -34,6 +40,6 @@ int	check_file(char *file_name)
 
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
-		return (perror(""), -1);
+		return (error_msg("Invalid file"));
 	return (0);
 }
